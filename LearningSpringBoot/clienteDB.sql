651715eac29dc1b74f97d9ac7650740ea83c5376
Freddy Machaca <freddyymachaca@gmail.com>
2024-07-07 22:21:22 -0400

    Finish Example LearningProject

---
+++ LearningSpringBoot/clienteDB.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE clienteDB;
+USE clienteDB;
+
+insert cliente values (1, 'Juan', 'Perez', 'juan@gmail.com');
+insert cliente values (2, 'Maria', 'Gomez', 'maria@gmail.com');
+
+select * from cliente;
